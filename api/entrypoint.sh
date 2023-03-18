25ebd9fd247482237466c82c48ebc5812ee588dd
S Piccolo <stevepic95@gmail.com>
2023-03-18 17:26:58 -0400

    new configs for ssh tunnel

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -3,11 +3,8 @@
 # Start the SSH agent
 eval "$(ssh-agent)"
 
-# Add the private key to the SSH agent
-echo "${PRIVATE_KEY}" | ssh-add -
-
 # Establish the SSH tunnel
-ssh -N -L ${SSH_TUNNEL_CLIENT_PORT}:${MYSQL_HOST_IP_OR_HOSTNAME}:${MYSQL_PORT} -p 22 ${SSH_USERNAME}@${MYSQL_HOST_IP_OR_HOSTNAME} &
+ssh -N -L ${SSH_TUNNEL_CLIENT_PORT}:${MYSQL_HOST_IP_OR_HOSTNAME}:${MYSQL_PORT} -p 22 -i /app/id_rsa ${SSH_USERNAME}@${MYSQL_HOST_IP_OR_HOSTNAME} &
 
 # Store the SSH tunnel process ID
 ssh_pid=$!
